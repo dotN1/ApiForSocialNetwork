5f053b792f8aa400155387fd9572931559da7eb8
Nikita Manzhosov <37019657+dotN1@users.noreply.github.com>
2022-05-26 22:18:12 +0300

    implementation basic routing

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-var express = require('express')
+var express = require("express");
 
-var app = express()
+var app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello API')
-})
+var items = [
+  {
+    name: "truealesya",
+    id: 24208,
+    uniqueUrlName: null,
+    photos: {
+      small: null,
+      large: null,
+    },
+    status: null,
+    followed: false,
+  },
+  {
+    name: "Shamaich",
+    id: 24207,
+    uniqueUrlName: null,
+    photos: {
+      small: null,
+      large: null,
+    },
+    status: null,
+    followed: false,
+  },
+  {
+    name: "Oleksandr_Kozak",
+    id: 24206,
+    uniqueUrlName: null,
+    photos: {
+      small: null,
+      large: null,
+    },
+    status: null,
+    followed: false,
+  },
+];
 
-app.listen(3012, function(){
-    console.log('API app started')
-})
+app.get("/", function (req, res) {
+  res.send("Hello API");
+});
+
+app.get("/users", function (req, res) {
+  res.send(items);
+});
+
+app.get("/users/:id", function (req, res) {
+  var user = items.find(function (user) {
+    return user.id === Number(req.params.id);
+  });
+  res.send(user)
+});
+
+app.listen(3012, function () {
+  console.log("API app started");
+});
