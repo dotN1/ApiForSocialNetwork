8326cd58beff7ec54b5d722298dc00696e7812e6
Nikita Manzhosov <37019657+dotN1@users.noreply.github.com>
2022-05-26 23:25:12 +0300

    implement adding and updating data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var items = [
   {
@@ -50,7 +54,38 @@ app.get("/users/:id", function (req, res) {
   var user = items.find(function (user) {
     return user.id === Number(req.params.id);
   });
-  res.send(user)
+  res.send(user);
+});
+
+app.post("/users", function (req, res) {
+  var user = {
+    name: req.body.name,
+    id: Date.now(),
+    uniqueUrlName: null,
+    photos: {
+      small: null,
+      large: null,
+    },
+    status: null,
+    followed: false,
+  };
+  items.push(user);
+  res.send(user);
+});
+
+app.put("/users/:id", function (req, res) {
+  var user = items.find(function (user) {
+    return user.id === Number(req.params.id);
+  });
+  user.name = req.body.name;
+  res.sendStatus(200);
+});
+
+app.delete("/users/:id", function (req, res) {
+  items = items.filter(function (user) {
+    return user.id !== Number(req.params.id);
+  });
+  res.sendStatus(200);
 });
 
 app.listen(3012, function () {
